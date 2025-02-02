71f8043831132b8635d85dfc552ea9d81da11b9c
Gulzar alice <107764457+Gulzaralice1@users.noreply.github.com>
2025-02-02 20:13:48 +0530

    update

--- Javascript_Project/Calender/script.js
+++ Javascript_Project/Calender/script.js
@@ -4,6 +4,7 @@ function updateTime(){
     const monthEl = document.getElementById("month");
     const yearEl = document.getElementById("year");
     const timeE1 = document.getElementById("time");
+    const demo = document.getElementById("demo");
 
     
     const today = new Date();
@@ -18,7 +19,20 @@ function updateTime(){
     monthEl.innerHTML = months[today.getMonth()];
     yearEl.innerHTML = today.getFullYear();
     timeE1.innerHTML = today.toLocaleTimeString();
-
     setTimeout(updateTime, 1000);
+}
+
+let time = 0; // Time in seconds
+
+function stopwatch() {
+    time++; // Increment time by 1 second
+    document.getElementById("demo").innerHTML = time; // Display seconds with leading zero
+    setTimeout(stopwatch, 1000); // Call the function again after 1 second
+}
+
+function start() {
+    if (time === 0) {
+        stopwatch(); // Start the stopwatch only if not already running
+    }
 }
 updateTime();
